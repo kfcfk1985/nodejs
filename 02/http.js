6fbc50519bb8ce4a95dff96cf2bead9a9ec635e1
ldf <kfcfk1985@163.com>
2020-08-29 19:34:37 +0800

    封装cors函数

--- 02/http.js
+++ 02/http.js
@@ -9,18 +9,21 @@ http
                 res.end(data);
             });
         } else if ((method == "GET" || method == "POST") && url == "/users") {
-            res.setHeader("Content-Type", "application/json");
-            res.setHeader("Access-Control-Allow-Origin", 'http://127.0.0.1:8080')
+            cros(res);
+            console.log('hello world')
             res.end(JSON.stringify([{
                 name: "tom",
                 age: 20
             }]));
         } else if (method == "OPTIONS" && url == "/users") {
-            res.writeHead(200, {
-                "Access-Control-Allow-Origin": "http://127.0.0.1:8080",
-                "Access-Control-Allow-Headers": "X-Token,Content-Type",
-            });
+            cros(res);
             res.end();
         }
     })
     .listen(3000);
+
+function cros(res) {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", 'http://127.0.0.1:8080');
+    res.setHeader("Access-Control-Allow-Headers", 'X-Token,Content-Type');
+}
