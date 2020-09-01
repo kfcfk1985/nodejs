2beb30ba166955e91bfa5a456c6d61cb73f1a175
ldf <kfcfk1985@163.com>
2020-09-01 19:36:47 +0800

    参考socket.io的官方例子重构服务器代码

--- 02/im/socket/index.js
+++ 02/im/socket/index.js
@@ -1,29 +1,24 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/index.html")
 });
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
   console.log('a user connected');
 
-  //响应某用户发送消息
-  socket.on('chat message', function (msg) {
-    console.log('chat message:' + msg);
-
-    // 广播给所有人
-    // io.emit('chat message', msg);
-    // 广播给除了发送者外所有人
-    socket.broadcast.emit('chat message', msg)
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    socket.broadcast.emit('chat message',msg);
   });
 
-  socket.on('disconnect', function () {
+  socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-http.listen(3000, function () {
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
