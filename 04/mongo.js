b0027a1da3e6d0dd2cd085840482a0ba25ab82a9
ldf <kfcfk1985@163.com>
2020-09-07 10:47:59 +0800

    更改代码

--- 04/mongo.js
+++ 04/mongo.js
@@ -1,11 +1,35 @@
+(async () => {
+    let dbs = {}; 
 
+    /*引入mongoDB*/
+    const MongoClient = require("mongodb").MongoClient;
 
+    /*实例化一个客户端 */
+    let client = new MongoClient(
+        "mongodb://localhost:27017", {
+            useNewUrlParser: true
+        }
 
+    )
 
+    /**建立连接 */
+    let ret = await client.connect();
+    //  console.log("---->ret",JSON.stringify(ret,null,2))
+    //  console.log("---->ret",(ret))
 
+    /* 创建 example 数据库*/    
+    dbs.exaplme = await client.db("eaxmple");
 
-(async()=>{
-    
+    /* 在 example 下创建 furits 表*/    
+    dbs.exaplme.furits = await dbs.exaplme.collection('furits')
 
+    /* 在 furits 表插入一条数据*/    
+    ret = dbs.exaplme.furits.insertOne({
+        name: '芒果',
+        price: '10'
+    })
+
+    /*更新数据*/
+    ret = await dbs.exaplme.furits.updateOne({ name: "芒果" }, { $set: { price: 19.8 } });
 
 })()
