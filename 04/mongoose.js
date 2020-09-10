7588bcc511930fe56d491b3eaaa5262199d23c90
ldf <kfcfk1985@163.com>
2020-09-10 16:26:25 +0800

    增加注析

--- 04/mongoose.js
+++ 04/mongoose.js
@@ -2,9 +2,10 @@
 const mongoose = require("mongoose");
 
 // 1.连接
-mongoose.connect("mongodb://localhost:27017/test",
-    { useNewUrlParser: true }
-);
+//mongoose.connect('mongodb://username:password@host:port/database?options...');
+mongoose.connect("mongodb://localhost:27017/test", {    
+    useNewUrlParser: true
+});
 
 const conn = mongoose.connection;
 conn.on("error", () => console.error("连接数据库失败"));
@@ -17,7 +18,7 @@ conn.once("open", async () => {
     });
 
     // 3.编译一个Model, 它对应数据库中复数、小写的Collection
-    const Model = mongoose.model("new", Schema);
+    const Model = mongoose.model("fruits", Schema);
     try {
         // 4.创建，create返回Promise
         let r = await Model.create({
@@ -29,15 +30,25 @@ conn.once("open", async () => {
 
         // 5.查询，find返回Query，它实现了then和catch，可以当Promise使用
         // 如果需要返回Promise，调用其exec()
-        r = await Model.find({ name: "苹果" });
+        r = await Model.find({
+            name: "苹果"
+        });
         console.log("查询结果:", r);
 
         // 6.更新，updateOne返回Query
-        r = await Model.updateOne({ name: "苹果" }, { $set: { name: '芒果' } });
+        r = await Model.updateOne({
+            name: "苹果"
+        }, {
+            $set: {
+                name: '芒果'
+            }
+        });
         console.log("更新结果：", r);
 
         // 7.删除，deleteOne返回Query
-        r = await Model.deleteOne({ name: "苹果" });
+        r = await Model.deleteOne({
+            name: "苹果"
+        });
         console.log("删除结果：", r);
     } catch (error) {
         console.log(error);
