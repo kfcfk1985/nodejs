6f768c70dd732d6ad2f53d5a6127c97ed8463fa5
ldf <kfcfk1985@163.com>
2020-09-11 19:28:02 +0800

    增加注析

--- 04/cart/models/user.js
+++ 04/cart/models/user.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
+// 定义 schema
 const schema = mongoose.Schema({
   name: String,
   password: String,
   cart: []
 });
 
+// 根据id查询返回指定数据
 schema.statics.getCart = function(_id) {
   return this.model("user")
     .findById(_id)
-    .exec();
+    .exec();      //exec() 执行完后返回Promise,好像find函数才需要这个操作
 };
 
+// 根据id更新指定数据
 schema.statics.setCart = function(_id, cart) {
   return this.model("user")
     .findByIdAndUpdate(_id, { $set: { cart } })
@@ -24,7 +27,7 @@ const model = mongoose.model("user", schema);
 model.updateOne(
   { _id: "5c1a2dce951e9160f0d8573b" },
   { name: "jerry", cart: [{ pname: "iPhone", price: 666, count: 1 }] },
-  { upsert: true },
+  { upsert: true },   // If set to true, creates a new document when no document matches the query criteria.
   (err, r) => {
     console.log('测试数据');
     console.log(err, r);
