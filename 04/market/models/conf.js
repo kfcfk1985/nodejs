3901654720125cc46b2f52071c1e2044e89931c3
ldf <kfcfk1985@163.com>
2021-03-01 22:26:43 +0800

    改为链接阿里云的mongoDB

--- 04/market/models/conf.js
+++ 04/market/models/conf.js
@@ -1,4 +1,6 @@
 module.exports = {
-    url: "mongodb://localhost:27017",
+    // url: "mongodb://localhost:27017",
+    // url: "mongodb://user42767089:Kfcfk851226@dds-f8zbtyo7vbbao288-pub.mongodb.rds.aliyuncs.com:3717/admin",
+    url: "mongodb://user42767089:Kfcfk851226@kfcfk1985.mongodb.rds.aliyuncs.com:3717/admin",
     dbName: 'test',
 }
